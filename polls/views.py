6f172fcf32663db19646dcbecf7802343cc7cb9d
ttpham <ttpham@otelo.fr>
2018-04-18 17:03:09 +0200

    fix import generic

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
