c6c63a01ca28c46ee1a4d1c68dd009684de7562b
TT <fathtus@gmail.com>
2018-05-01 11:33:29 +0200

    add books models

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'books.apps.BooksConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
