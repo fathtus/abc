f0509682fbbb047cf0b8de660611af31bf52f10b
ttpham <ttpham@otelo.fr>
2018-05-03 16:36:39 +0200

    add resto

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'resto.apps.RestoConfig',
     'press.apps.PressConfig',
     'books.apps.BooksConfig',
     'polls.apps.PollsConfig',
