2704443c6c713c905ff356f897bd084d1942da12
TT <fathtus@gmail.com>
2018-05-08 23:11:37 +0200

    add form django

--- welcome/views.py
+++ welcome/views.py
@@ -3,9 +3,12 @@ import math
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
 
 from . import database
 from .models import PageView
+from .forms import NameForm
 
 # Create your views here.
 
@@ -84,7 +87,23 @@ def calculate(request):
         'yy': yy
     }) 
 
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
 
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
 
 
 def jdFromDate(dd, mm, yy):
