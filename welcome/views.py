dc0ff527f5619bbec9e73fcad96035656cc22094
TT <fathtus@gmail.com>
2018-05-09 22:31:37 +0200

    change file

--- welcome/views.py
+++ welcome/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponseRedirect
 
 from . import database
 from .models import PageView
-from . import NameForm
+from .forms import NameForm
 
 # Create your views here.
 
