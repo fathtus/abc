69f298d84460f6e9e75434406a094610438f5df2
TT <fathtus@gmail.com>
2018-05-11 00:44:15 +0200

    bouger

--- welcome/models.py
+++ welcome/models.py
@@ -9,30 +9,30 @@ class PageView(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
-# TITLE_CHOICES = (
-#     ('MR', 'Mr.'),
-#     ('MRS', 'Mrs.'),
-#     ('MS', 'Ms.'),
-# )
+TITLE_CHOICES = (
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+)
 
-# class Author(models.Model):
-#     name = models.CharField(max_length=100)
-#     title = models.CharField(max_length=3, choices=TITLE_CHOICES)
-#     birth_date = models.DateField(blank=True, null=True)
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    birth_date = models.DateField(blank=True, null=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Book(models.Model):
-#     name = models.CharField(max_length=100)
-#     authors = models.ManyToManyField(Author)
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
 
-# class AuthorForm(ModelForm):
-#     class Meta:
-#         model = Author
-#         fields = ['name', 'title', 'birth_date']
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ['name', 'title', 'birth_date']
 
-# class BookForm(ModelForm):
-#     class Meta:
-#         model = Book
-#         fields = ['name', 'authors']
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'authors']
